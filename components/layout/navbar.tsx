41dfd35353751d7a2621dc28f21e17e0002b5afd
r.sato <satoramirodev@gmail.com>
2025-04-13 15:04:27 -0300

    feat: add z-index to mobile menu for improved visibility

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -116,7 +116,7 @@ export default function Navbar() {
                 </div>
                 {isMenuOpen && (
                   <div
-                    className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10"
                     role="menu"
                     aria-orientation="vertical"
                     aria-labelledby="user-menu-button"
